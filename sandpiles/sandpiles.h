89d3879a9477fee9453fe4e536ba6389e63c4a49
leila7575 <leila.louajri@gmail.com>
2025-02-02 19:18:24 +0100

    sandpiles.h

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void print_before_toppling(int grid[3][3]);
 
