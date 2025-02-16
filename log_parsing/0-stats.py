ad20e12de668c5863689a3dcb796210b6f942cde
leila7575 <leila.louajri@gmail.com>
2025-02-16 19:22:32 +0100

    0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,39 +3,43 @@
 
 import sys
 
-
 total_size = 0
 status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
 status_code_number = {}
 line_number = 0
 
+def print_stats():
+    """Prints statistics."""
+    print(f"File size: {total_size}")
+    for i in sorted(status_code_number.keys()):
+        print(f"{i}: {status_code_number[i]}")
+
 try:
     for line in sys.stdin:
         elements = line.split()
 
-        file_size = int(elements[-1])
-        status_code = int(elements[-2])
+
+        if len(elements) < 7:
+            continue
+
+        try:
+            file_size = int(elements[-1])
+            status_code = int(elements[-2])
+        except ValueError:
+            continue 
 
         total_size += file_size
 
         if status_code in status_codes:
-            try:
-                status_code_number[status_code] += 1
-            except KeyError:
-                status_code_number[status_code] = 1
+            status_code_number[status_code] = status_code_number.get(status_code, 0) + 1
 
         line_number += 1
 
         if line_number % 10 == 0:
-            print(f"File size: {total_size}")
-            for i in sorted(status_code_number.keys()):
-                print(f"{i}: {status_code_number[i]}")
+            print_stats()
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    print(f"File size: {total_size}")
-    for i in sorted(status_code_number.keys()):
-        print(f"{i}: {status_code_number[status_code]}")
-    raise
+    print_stats()  
