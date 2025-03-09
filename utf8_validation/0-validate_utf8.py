f5e7726b754f373b9aeb93fd8f1debc3998e0099
leila7575 <leila.louajri@gmail.com>
2025-03-09 17:06:11 +0100

    adding 0-va0-validate_utf8 file

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module contains the function validUTF8."""
+
+
+def validUTF8(data):
+    """Checks if data set presents valid UTF_8 encoding."""
+    bytes_number = 0
+    for byte in data:
+        if bytes_number:
+            if byte >> 6 != 0b10:
+                return False
+            bytes_number -= 1
+        else:
+            if byte >> 7 == 0:
+                bytes_number = 0
+            elif byte >> 5 == 0b110:
+                bytes_number = 1
+            elif byte >> 4 == 0b1110:
+                bytes_number = 2
+            elif byte >> 3 == 0b11110:
+                bytes_number = 3
+            else:
+                return False
+    return bytes_number == 0
