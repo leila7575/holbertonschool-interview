65014240dbe20e0a3986479bb9e08066dd67f5cc
leila7575 <leila.louajri@gmail.com>
2025-07-31 11:09:50 +0200

    updated wildcmp function to match no loop requirement

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -18,8 +18,8 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
-			s2++;
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
 
 		if (wildcmp(s1, s2 + 1))
 			return (1);
